122e5fd5f2a14b3e0277eb3d2c2bdeffa00584e2
Joan Casimero <111473276+joancasimero@users.noreply.github.com>
2022-10-03 11:23:34 +0800

    Add files via upload

---
+++ Assignment 2.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 5; i != 0; i--) {
+            System.out.println("Joan");
+
+        if (i == 4) {
+                System.out.println("Break!");
+                break;
+                
+        }
+
+        int lastnameCount = 5;
+
+        while (lastnameCount != 0) {
+            System.out.println("Casimero");
+            lastnameCount--;
+
+        if (i == 3) {
+                System.out.println("Go ahead i will skip this third loop. i will ride on the next loop!");
+                continue;
+                
+        }
+
+        int nicknameCount = 5;
+
+        do {
+            System.out.println("Jon");
+            nicknameCount--;
+        } while (nicknameCount != 0);
+        }
+        }
+    }
+}
